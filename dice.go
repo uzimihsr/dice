d8306d8d6fcdb542d603484c03db9b55d86d8127
uzimihsr <uzimihsr@gmail.com>
2019-09-03 23:00:01 +0900

    add option

--- dice.go
+++ dice.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	n := 6
+	// コマンドラインオプションで与える値の変数定義
+	var (
+		faces int
+		dices int
+	)
+
+	// コマンドラインオプションの設定
+	flag.IntVar(&faces, "f", 6, "The number of dice faces")
+	flag.IntVar(&dices, "d", 1, "The number of dices to throw")
+	flag.Parse()
+
+	// サイコロを振り, 出目を出力
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(rand.Intn(n) + 1)
+	for i := 0; i < dices; i++ {
+		fmt.Printf("%d ", (rand.Intn(faces) + 1))
+	}
+	fmt.Println()
 }
