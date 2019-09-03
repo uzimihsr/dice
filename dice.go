1dcdfeba9d9516d98a95ab0812a0a2dbf4facfdd
uzimihsr <uzimihsr@gmail.com>
2019-09-03 23:55:45 +0900

    use uint

--- dice.go
+++ dice.go
@@ -10,19 +10,19 @@ import (
 func main() {
 	// コマンドラインオプションで与える値の変数定義
 	var (
-		faces int
+		faces uint
 		dices int
 	)
 
 	// コマンドラインオプションの設定
-	flag.IntVar(&faces, "f", 6, "The number of dice faces")
+	flag.UintVar(&faces, "f", 6, "The number of dice faces")
 	flag.IntVar(&dices, "d", 1, "The number of dices to throw")
 	flag.Parse()
 
 	// サイコロを振り, 出目を出力
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < dices; i++ {
-		fmt.Printf("%d ", (rand.Intn(faces) + 1))
+		fmt.Printf("%d ", (rand.Intn(int(faces)) + 1))
 	}
 	fmt.Println()
 }
